72a7597091f00c07f662d45a6f4945d637bd45ee
Artem Kyznecov <soldatenco96@mail.ru>
2022-05-03 17:58:37 +0300

    solutions

--- work_logical_opertors.py
+++ work_logical_opertors.py
@@ -1,6 +1,11 @@
-gender = "М"
-age = 20
-if (gender == "М" or gender == "Ж") and 18<=age<=27:
+gender = input("Ваш пол (М или Ж): ")
+
+while gender != "М" and gender != "Ж":
+    print("Некорректное значение!")
+    gender = input("Попробуйте еще раз: ")
+
+age = int(input("Ваш возраст: "))
+if (gender == "М" or gender == "Ж") and 18 <= age <= 27:
     print("Кандидант подходит")
 else:
     print("Кандидант не подходит")
